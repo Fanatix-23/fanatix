8aeee05268a6b9a0ad46555dfce4eebd1eb05358
DISHA DWIVEDI <dishadwivedi123g@gmai.com>
2023-12-09 09:56:15 +0530

    mantle_testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,6 +8,7 @@ const API_URL_MAINNET = process.env.API_URL_MAINNET;
 const API_URL_SMART_CHAIN_TESTNET = process.env.API_URL_SMART_CHAIN_TESTNET;
 const API_URL_SMART_CHAIN = process.env.API_URL_SMART_CHAIN;
 const API_URL_SEPOLIA = process.env.API_URL_SEPOLIA;
+const API_URL_MANTLE_TESTNET = process.env.API_URL_MANTLE_TESTNET;
 
 module.exports = {
   solidity: {
@@ -31,6 +32,11 @@ module.exports = {
          url : API_URL_MAINNET,
          accounts : [`0x${PRIVATE_KEY}`],
          chainId : 137,
+      },
+      mantle_testnet : {
+        url : API_URL_MANTLE_TESTNET,
+        accounts : [`0x${PRIVATE_KEY}`],
+         chainId : 5001,
       }
     },
   contractSizer: {
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,10 @@
 
 const {ethers} = require("hardhat");
-
+// contract address -  0x179381D8A575D50d9EE6f74CD3F486F7D6A0EFc0
 async function main() {
-  const nft = await ethers.getContractFactory("abc");
+  const nft = await ethers.getContractFactory("fanatix");
   const contract = await nft.deploy();
-  console.log(contract);
+  console.log(await contract.getAddress);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
